8ddeaab9c9d88907c29fa67f1c8af2d564bd1217
MinhazRaju <you@example.com>
2023-08-26 21:46:43 +0600

    '9.46'

--- frontend/src/screens/_Product.js
+++ frontend/src/screens/_Product.js
@@ -21,22 +21,23 @@ export default connect(mapStateToProps)(class Product extends React.Component {
         store.dispatch(SingleProductAction(32))
         this.state = {
             imagePath: null,
-            thumbnail: null
+            thumbnail: null,
+            variationthumb: null
         }
     }
 
 
     componentDidMount() {
-        setTimeout(this.test2, 3000)
+        setTimeout(this.thumbimage, 3000)
 
     }
-    test = (e) => {
+    productimage = (e) => {
 
-        console.log("fire", e.target.getAttribute('src'), e)
+
         this.setState({ imagePath: e.target.getAttribute('src') })
     }
 
-    test2 = () => {
+    thumbimage = () => {
 
         if (this.props.SingleProductData) {
             this.setState({ imagePath: `../assets/static${this.props.SingleProductData.image[0].photo}` })
@@ -56,18 +57,32 @@ export default connect(mapStateToProps)(class Product extends React.Component {
 
 
         this.setState({ thumbnail: x })
+    }
 
 
+    variationimage = (id) => {
 
+        const x = this.props.SingleProductData.variation.filter(data => data.id == id)
 
+        let y = []
+        x.map((data) => {
+            data.variation_image.map((data) => {
+                y.push(data)
+            })
+        })
 
+        this.setState({ variationthumb: y })
 
+    }
+
+    closethumb = () => {
+        this.setState({ variationthumb: null })
     }
 
 
     render() {
 
-        console.log(this.props.SingleProductData, this.state.thumbnail)
+        console.log(this.props.SingleProductData, this.state.thumbnail, this.state.variationthumb)
 
 
 
@@ -97,7 +112,7 @@ export default connect(mapStateToProps)(class Product extends React.Component {
                 <section class="mt-8">
                     <div class="container">
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <div class="slider slider-for">
                                     <div class="test">
 
@@ -127,6 +142,7 @@ export default connect(mapStateToProps)(class Product extends React.Component {
 
 
                                 </div>
+                                <br />
                                 <div >
 
                                     <Carousel cols={6} rows={1} gap={10} hideArrow={this.state.thumbnail == null ? true : this.state.thumbnail.length < 6 ? true : false} loop>
@@ -134,7 +150,7 @@ export default connect(mapStateToProps)(class Product extends React.Component {
                                         {this.state.thumbnail == null ? null : this.state.thumbnail.map((data) => {
                                             return (
                                                 <Carousel.Item>
-                                                    <img onMouseOver={this.test} width="100%" src={`../assets/static${data.photo}`} />
+                                                    <img onMouseOver={this.productimage} width="100%" src={`../assets/static${data.photo}`} />
                                                 </Carousel.Item>
                                             )
 
@@ -145,53 +161,92 @@ export default connect(mapStateToProps)(class Product extends React.Component {
                                 </div>
                             </div>
 
-                            <div class="col-md-6">
-                                <div class="ps-lg-10 mt-6 mt-md-0">
+                            {this.props.SingleProductData == undefined ? null :
+
+                                (
+                                    <div class="col-md-4">
+                                        <div class="ps-lg-10 mt-6 mt-md-0">
 
 
-                                    <h1 class="mb-1">Napolitanke Ljesnjak </h1>
-                                    <div class="mb-4">
-                                        <small class="text-warning"> <i class="bi bi-star-fill"></i>
-                                            <i class="bi bi-star-fill"></i>
-                                            <i class="bi bi-star-fill"></i>
-                                            <i class="bi bi-star-fill"></i>
-                                            <i class="bi bi-star-half"></i></small><a href="#" class="ms-2">(30 reviews)</a></div>
-                                    <div class="fs-4">
-                                        <span class="fw-bold text-dark">$32</span> <span
-                                            class="text-decoration-line-through text-muted">$35</span><span><small class="fs-6 ms-2 text-danger">26%
-                                                Off</small></span>
-                                    </div>
+                                            <h1 class="mb-1">{this.props.SingleProductData.title} </h1>
+                                            <div class="mb-4">
+                                                <small class="text-warning"> <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-fill"></i>
+                                                    <i class="bi bi-star-half"></i></small><a href="#" class="ms-2">(30 reviews)</a></div>
+                                            <div class="fs-4">
+                                                <span class="fw-bold text-dark">$32</span> <span
+                                                    class="text-decoration-line-through text-muted">$35</span><span><small class="fs-6 ms-2 text-danger">26%
+                                                        Off</small></span>
+                                            </div>
 
-                                    <hr class="my-6" />
-                                    <div class="mb-5">
-                                        <button type="button" class="btn btn-outline-secondary">250g</button>
+                                            <hr class="my-6" />
+                                            <div class="mb-5">
 
-                                    </div>
-                                    <div>
+                                                {this.props.SingleProductData.variation.map((data) => {
+                                                    return (
+                                                        <>
+                                                            <button onClick={() => this.variationimage(data.id)} type="button" class="btn btn-outline-secondary">{data.color}</button>&nbsp;
+                                                        </>
+
+                                                    )
+
+
+                                                })}
+
+
+                                                <hr class="my-6" />
+
+                                                <div >
+                                                    {this.state.variationthumb == undefined ? null : this.state.variationthumb.map((data) => {
+                                                        return (
+                                                            <>
+
+                                                                <img onMouseOver={this.productimage} width="20%" height="30%" src={`../assets/static${data.photo}`} />&nbsp;
+                                                            </>
+                                                        )
+                                                    })}
+
+
+                                                </div>
+
+                                                {this.state.variationthumb == null ? null : (<button onClick={this.closethumb} type="button" class="btn-close me-2 m-auto" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>)}
+
+                                            </div>
+
+                                            <div>
 
 
 
-                                        <div class="input-group input-spinner  ">
-                                            <input type="button" value="-" class="button-minus  btn  btn-sm " data-field="quantity" />
-                                            <input type="number" step="1" max="10" value="1" name="quantity" class="quantity-field form-control-sm form-input   " />
-                                            <input type="button" value="+" class="button-plus btn btn-sm " data-field="quantity" />
+                                                <div class="input-group input-spinner  ">
+                                                    <input type="button" value="-" class="button-minus  btn  btn-sm " data-field="quantity" />
+                                                    <input type="number" step="1" max="10" value="1" name="quantity" class="quantity-field form-control-sm form-input   " />
+                                                    <input type="button" value="+" class="button-plus btn btn-sm " data-field="quantity" />
+                                                </div>
+
+                                            </div>
+                                            <div class="mt-3 row justify-content-start g-2 align-items-center">
+
+                                                <div class="col-xxl-4  d-grid">
+
+                                                    <button type="button" class="btn btn-primary"><i class="feather-icon icon-shopping-bag me-2"></i>Add to
+                                                        cart</button>
+                                                </div>
+
+                                            </div>
+
+                                            <hr class="my-6" />
+
                                         </div>
-
                                     </div>
-                                    <div class="mt-3 row justify-content-start g-2 align-items-center">
+                                )
 
-                                        <div class="col-xxl-4 col-lg-4 col-md-5 col-5 d-grid">
+                            }
 
-                                            <button type="button" class="btn btn-primary"><i class="feather-icon icon-shopping-bag me-2"></i>Add to
-                                                cart</button>
-                                        </div>
 
-                                    </div>
 
-                                    <hr class="my-6" />
 
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </section>
